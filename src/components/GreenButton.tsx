3db5b6e5481b4021514a0757af0367ab33a8b0ee
petergok <petergok@hotmail.com>
2026-04-15 16:32:50 -0400

    Add green button component and canvas storyboard

---
+++ src/components/GreenButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export function GreenButton() {
+  return (
+    <Button className="bg-green-600 hover:bg-green-700 text-white">
+      Green Button
+    </Button>
+  );
+}
